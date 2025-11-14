102305281bdf2213ffef82e2b1efaf349e1c9ab3
Igo Carvalho <igocarvalho00@gmail.com>
2025-11-13 21:09:29 -0300

    feat: handle application errors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,23 @@
 import { fastify } from 'fastify';
+import { ZodError, z } from 'zod';
+import { env } from './env';
 import { appRoutes } from './http/routes';
 
 export const app = fastify();
 
 app.register(appRoutes);
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error', issues: z.treeifyError(error) });
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    // biome-ignore lint/suspicious/noConsole: Show error in development
+    console.error(error);
+  }
+
+  return reply.status(500).send({ message: 'Internal server error' });
+});
