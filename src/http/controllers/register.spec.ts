535509760d358eb021a183c2e9c318148ebaefdf
Igo Carvalho <igocarvalho00@gmail.com>
2025-11-27 20:07:14 -0300

    test: create register and authenticate e2e tests

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,24 @@
-import { expect, test } from 'vitest';
+import { StatusCodes } from 'http-status-codes';
+import request from 'supertest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import { app } from '@/app';
 
-test('register', () => {
-  expect(1 + 1).toBe(2);
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to register a user', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456',
+    });
+
+    expect(response.statusCode).toBe(StatusCodes.CREATED);
+  });
 });
